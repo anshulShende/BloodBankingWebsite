1daf5d3643573c1b976d13310e31b249bcda68a4
Samiksha Wartak <samikshawartak@gmail.com>
2020-04-07 22:04:21 +0530

    Admin added

---
+++ admin.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"
+        integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <title>Admin</title>
+</head>
+
+<body>
+    <!-- topnav -->
+    <nav class="navbar navbar-expand-sm bg-danger navbar-dark fixed-top">
+        <a class="navbar-brand" href="index.php">My Blood Bank</a>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="admin.php"><i class="fa fa-user" aria-hidden="true"></i>&nbsp; Admin</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">&ensp;<i class="fa fa-users" aria-hidden="true"></i>&nbsp;Users</a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- form  -->
+    <div class="container-fluid form1 p-4 border border-dark col-md-4 offset-4">
+        <form class="" action="#">
+            <div class="form-group">
+                <label for="email">Enter Username:</label>
+                <input type="email" name="un" class="form-control" placeholder="Username">
+            </div>
+            <div class="form-group">
+                <label for="pwd">Enter Password:</label>
+                <input type="password" name="ps" class="form-control" placeholder="Password" >
+            </div>
+            <button type="submit" name="sub" class="btn btn-outline-danger col-md-3 offset-4">Login</button>
+        </form>
+    </div>
+
+
+
+    <!-- bottomnav -->
+    <nav class="navbar navbar-expand-sm bg-danger navbar-dark fixed-bottom">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#"> <i class="fa fa-facebook" aria-hidden="true"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            </li>
+            <li>
+                <a class="nav-link" href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+            </li>
+
+        </ul>
+    </nav>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Admin Login</title>
+  <title>Blood Bank</title>
 
-	<meta charset="utf-8">
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -18,15 +18,15 @@
 </head>
 <body>
 
-	<!-- topnav -->
-	<nav class="navbar navbar-expand-sm bg-danger navbar-dark fixed-top">
-  <a class="navbar-brand" href="#">My Blood Bank</a>
+  <!-- topnav -->
+  <nav class="navbar navbar-expand-sm bg-danger navbar-dark fixed-top">
+  <a class="navbar-brand" href="index.php">My Blood Bank</a>
   <ul class="navbar-nav ml-auto">
     <li class="nav-item">
-      <a class="nav-link" href="#"><i class="fa fa-user" aria-hidden="true"></i>&nbsp; Admin</a>
+      <a class="nav-link" href="admin.php"><i class="fa fa-user" aria-hidden="true"></i>&nbsp; Admin</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#"><i class="fa fa-users" aria-hidden="true"></i>&nbsp;Users</a>
+      <a class="nav-link" href="#">&ensp;<i class="fa fa-users" aria-hidden="true"></i>&nbsp;Users</a>
     </li>
   </ul>
 </nav>
@@ -66,21 +66,21 @@
 
 <!-- bottomnav -->
 <nav class="navbar navbar-expand-sm bg-danger navbar-dark fixed-bottom">
-	<ul class="navbar-nav">
+  <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link" href="#">	<i class="fa fa-facebook" aria-hidden="true"></i></a>
+      <a class="nav-link" href="#"> <i class="fa fa-facebook" aria-hidden="true"></i></a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
     </li>
     <li>
-    	<a class="nav-link" href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+      <a class="nav-link" href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
     </li>
 
   </ul>
-	
-	
-	
+  
+  
+  
 
 </nav>
 </body>
