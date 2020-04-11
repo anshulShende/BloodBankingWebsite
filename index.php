0e56daf58bb51b3b459450fa1146971e0a1cdc5d
Samiksha Wartak <samikshawartak@gmail.com>
2020-04-11 23:09:32 +0530

    homepage updated

--- index.php
+++ index.php
@@ -25,7 +25,8 @@
   <?php include 'navbar.php';?>
 
 <!-- carousel -->
-<div id="demo" class="carousel slide" data-ride="carousel" data-interval="3000">
+<div class="row">
+<div id="demo" class="carousel slide col-sm-6" data-ride="carousel" data-interval="3000">
 
   <ul class="carousel-indicators">
     <li data-target="#demo" data-slide-to="0" class="active"></li>
@@ -37,19 +38,19 @@
 
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="image/b4.jpg" alt="Blood Groups">
+      <img src="image/b4.jpg" alt="Blood Groups" class="border border-dark">
     </div>
     <div class="carousel-item">
-      <img src="image/b5.jpg" alt="Blood Bank">
+      <img src="image/b5.jpg" alt="Blood Bank" class="border border-dark">
     </div>
     <div class="carousel-item">
-      <img src="image/b6.jpg" alt="Blood Donation">
+      <img src="image/b6.jpg" alt="Blood Donation" class="border border-dark">
     </div>
     <div class="carousel-item" >
-      <img src="image/b2.png" alt="Blood Donation">
+      <img src="image/b2.png" alt="Blood Donation" class="border border-dark">
     </div>
     <div class="carousel-item">
-      <img src="image/b1.jpg" alt="Doctors">
+      <img src="image/b1.jpg" alt="Doctors" class="border border-dark">
     </div>
   </div>
 
@@ -62,7 +63,63 @@
 
 </div>
 
+<div class="container-fluid p-4 col-sm-4" style="font-size: 30px; text-align: justify;"><br>
+  <p><h2>About Us</h2>
+  My Blood Bank is a website which allows users to login and book an appointment for Blood Donation
+   and also request for blood when needed. This website also gives information to users regarding Blood Groups, 
+   Blood Donation and some related tips. </p>
+</div>
+</div>
 
+<a href="#section1" class="col-sm-1 offset-5" style="font-size: 20px; color: white; text-decoration: none;"><i class="fas fa-chevron-down"></i>
+  &nbsp;View More</a>
+<br><br><br>
+<div class="row">
+  <div class="col">
+<div class="container" id="section1">
+  <div class="card" style="width:400px">
+    <i class="fas fa-syringe text-center" style="font-size: 50px; color: red;"></i>
+    <div class="card-body">
+      <h4 class="card-title text-center">Blood Groups</h4>
+      <p class="card-text" style="text-align: justify;">There are four main blood groups – A, B, AB and O.       
+      Each group can be either RhD positive or RhD negative, which means in total there are eight main blood groups.</p>
+    </div>
+  </div>
+</div>
+  </div>
+<div class="col">
+  <div class="container" id="section1">
+    <div class="card" style="width:400px">
+      <i class="fas fas fa-tint text-center" style="font-size: 50px; color: red;"></i>
+      <div class="card-body">
+        <h4 class="card-title text-center">Who can give Blood</h4>
+        <ul class="card-text"><li>Are fit and healthy</li>
+         <li>Weigh over 45-50kg</li>
+        <li>Are aged between 18 and 66</li>
+      <li>Not pregnant</li></ul>
+      </div>
+    </div>
+  </div>
+</div>
+  <div class="col">
+    <div class="container" id="section1">
+      <div class="card" style="width:400px">
+        <i class="fas fa-plus text-center" style="font-size: 50px; color: red;"></i>
+        <div class="card-body">
+          <h4 class="card-title text-center">Tips for Blood Donation</h4>
+          <ul class="card-text">
+            <li>Get a good night’s sleep</li>
+            <li>Eat a balanced meal and avoid fatty foods</li>
+            <li>Drink plenty of fluids before and after</li>
+            <li>In case of uneasiness, call the Doctor</li>
+          </ul>
+        </div>
+      </div>
+      </div>
+    </div>
+</div>
+
+<br><br><br>
 <!-- bottomnav -->
 <?php include 'footer.php';?>
 </body>
